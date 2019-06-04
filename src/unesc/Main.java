e2bd484eeea730f6aa87d99e8216d65d3e36efc0
gugaburigo29 <gugatre32123>
2019-06-04 19:38:45 -0300

    Corrigido inversão da pilha de tokens

--- src/unesc/Main.java
+++ src/unesc/Main.java
@@ -3,36 +3,36 @@ package unesc;
 
 import unesc.analisador.AnalisadorSintatico;
 import unesc.analisador.AnalisadorLexico;
+
 import java.util.Stack;
 
 public class Main {
 
     //- Programa a ser analisado
-    private String programa = 
+    private String programa =
             "PROGRAM TESTE ; "
-            + "VAR "
-            + "   X , Y , Z : INTEGER ; "
-            + "BEGIN "
-            + "   X := 10 ; "
-            + "   Y := 20 ; "
-            + "   Z := 30 ; "
-            + "  REPEAT "
-            + "    BEGIN "
-            + "      WRITELN ( X , Y , Z ) ; "
-            + "    END "
-            + "  UNTIL X > 10 ; "
-            + "END . "
-            ;
-    
+                    + "VAR "
+                    + "   X , Y , Z : INTEGER ; "
+                    + "BEGIN "
+                    + "   X := 10 ; "
+                    + "   Y := 20 ; "
+                    + "   Z := 30 ; "
+                    + "  REPEAT "
+                    + "    BEGIN "
+                    + "      WRITELN ( X , Y , Z ) ; "
+                    + "    END "
+                    + "  UNTIL X > 10 ; "
+                    + "END . ";
+
     /**
-     Veja as instruções dentro dos métodos gerarTokens() e analisar()
+     * Veja as instruções dentro dos métodos gerarTokens() e analisar()
      */
     public void iniciar() {
-        
+
         //Objetivo 1: Transformar o conteudo do "programa" em uma pilha de tokens.
         Stack<String> tokens = new AnalisadorLexico()
                 .gerarTokens(programa);
-        
+
         //Objetivo 2: Dentro do método analisar() escrever o algorítimo de contido no pseudocódigo 
         //            para verificar se a estrutura da linguagem definida na variável programa está correta
         //new AnalisadorSintatico()
--- src/unesc/analisador/AnalisadorLexico.java
+++ src/unesc/analisador/AnalisadorLexico.java
@@ -2,25 +2,19 @@ package unesc.analisador;
 
 
 import java.util.Stack;
-import java.util.Collections;
 
 public class AnalisadorLexico {
 
     public Stack<String> gerarTokens(String programa) {
         String[] tokens = programa.split(" ");
         Stack<String> pilhaTokens = new Stack<>();
-        Stack<String> pilhaInvertida = new Stack<>();
-        
-        for (int i = 0; i < tokens.length; i++) {
+
+        for (int i = tokens.length - 1; i >= 0; i--) {
             String token = tokens[i];
             pilhaTokens.push(token);
         }
-        
-        for (int i = pilhaTokens.size(); i > 0; i--) {
-            pilhaInvertida.push(pilhaTokens.pop());
-        }
-        
-        return pilhaInvertida;
+
+        return pilhaTokens;
     }
 
 }
